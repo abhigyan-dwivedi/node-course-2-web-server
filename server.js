dd2333b39fd596d53d04ec07d6d8e5b74af30aee
Abhigyan Dwivedi <abhigyan.dwivedi@gmail.com>
2017-03-25 03:42:41 +0530

    Setup server script and heroku port for heroku commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express')
 const hbs=require('hbs')
 const fs=require('fs')
 
-const port=process.env.PORT;
+const port=process.env.PORT||3000;
 
 app=express();
 
@@ -69,5 +69,5 @@ app.get('/bad',(req,res)=>{
 
 
 app.listen(port,()=>{
-  console.log('Server is up on port:3000');
+  console.log(`Server is up on port:${port}`);
 });
